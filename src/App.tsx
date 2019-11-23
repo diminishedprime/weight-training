6a60f51498ceaef6db69aec9dfeabe875131dfe5
Matt Hamrick <matthewjhamrick@gmail.com>
2019-11-23 04:31:46 +0000

    Added firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
 import React from 'react';
 import './App.sass'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBBu2D-owaz14CfZvmOqjSoN0oMde5D5NE",
+    authDomain: "weight-training-8a1ac.firebaseapp.com",
+    databaseURL: "https://weight-training-8a1ac.firebaseio.com",
+    projectId: "weight-training-8a1ac",
+    storageBucket: "weight-training-8a1ac.appspot.com",
+    messagingSenderId: "21223491336",
+    appId: "1:21223491336:web:7378ae65a038e84eda8ebd",
+    measurementId: "G-4F9TH5XYE6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const App: React.FC = () => {
   return (
