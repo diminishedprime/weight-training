64d554552a0f370ddca460e2b48f5a30da4988ca
Matt Hamrick <matthewjhamrick@gmail.com>
2019-11-23 19:00:01 +0000

    Added simple nav-bar & ability to sign out.

--- src/Home.tsx
+++ src/Home.tsx
@@ -22,18 +22,59 @@ export default () => {
         date: new Date()
       });
   }, [user]);
+  const [navActive, setNavActive] = React.useState(false);
+
+  const signOut = React.useCallback(() => {
+    firebase.auth().signOut();
+  }, []);
 
   if (user === null) {
     return <div>Checking login status</div>;
   }
 
   return (
-    <div>
-      You're logged in as {user.email}
-      <LiftTable liftType={t.LiftType.DEADLIFT} user={user} />
-      <button className="button" onClick={addRandomLift}>
-        Add random lift
-      </button>
-    </div>
+    <>
+      <nav
+        className="navbar is-fixed-top"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <div className="title">Weight Training</div>
+          </a>
+
+          <a
+            role="button"
+            className={`navbar-burger burger ${navActive && "is-active"}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={() => {
+              setNavActive(old => !old);
+            }}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div className={`navbar-menu ${navActive && "is-active"}`}>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <a className="button is-primary" onClick={signOut}>
+                Sign Out
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div>
+        <LiftTable liftType={t.LiftType.DEADLIFT} user={user} />
+        <button className="button" onClick={addRandomLift}>
+          Add random lift
+        </button>
+      </div>
+    </>
   );
 };
