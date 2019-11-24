a1cdcee0d1c815cfb226a1474f1a045dd64c5eed
Matt Hamrick <matthewjhamrick@gmail.com>
2019-11-24 20:43:55 +0000

    Updates

---
+++ precache-manifest.bc86a69e996efd060866318f1dcfb8bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5396010badd6135729dfa4a9097015b",
+    "url": "/weight-training/index.html"
+  },
+  {
+    "revision": "6cc1201372a601dda114",
+    "url": "/weight-training/static/css/main.99109b8f.chunk.css"
+  },
+  {
+    "revision": "760164cb96daad06b222",
+    "url": "/weight-training/static/js/2.aefbacb8.chunk.js"
+  },
+  {
+    "revision": "6cc1201372a601dda114",
+    "url": "/weight-training/static/js/main.76d39737.chunk.js"
+  },
+  {
+    "revision": "652d4d0b56745d89a163",
+    "url": "/weight-training/static/js/runtime-main.d246dc47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "diminishedprime.github.io/weight-training/precache-manifest.745127ac216c2f92c2a7328ed62f1824.js"
+  "/weight-training/precache-manifest.bc86a69e996efd060866318f1dcfb8bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("diminishedprime.github.io/weight-training/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weight-training/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
