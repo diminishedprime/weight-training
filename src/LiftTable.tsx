5860eae2a8ddbc2f11c95846f2426d79a5e8ea77
Matt Hamrick <matthewjhamrick@gmail.com>
2019-12-07 05:46:00 +0000

    Added a count-up timer since last lift if it's happened recently.

--- src/LiftTable.tsx
+++ src/LiftTable.tsx
@@ -3,6 +3,54 @@ import firebase from "firebase/app";
 import * as t from "./types";
 import * as db from "./db";
 import { Link } from "react-router-dom";
+import moment from "moment";
+
+const TimeSince = ({ time }: { time: Date }) => {
+  const [displayTime, setDisplayTime] = React.useState(
+    time.toLocaleTimeString()
+  );
+  const [timeClass, setTimeClass] = React.useState("");
+  React.useEffect(() => {
+    const nowMoment = moment.utc();
+    const timeUtcMoment = moment(time.toUTCString());
+    const timeSinceLift = moment.duration(
+      nowMoment.diff(timeUtcMoment, "milliseconds"),
+      "milliseconds"
+    );
+    const interval = setInterval(() => {
+      timeSinceLift.add(1, "second");
+      const minutes = timeSinceLift.minutes();
+      if (minutes >= 15 || timeSinceLift.asMinutes() >= 15) {
+        setDisplayTime(time.toLocaleTimeString());
+        setTimeClass("");
+        clearInterval(interval);
+        return;
+      }
+      if (minutes < 2) {
+        setTimeClass(old => {
+          if (old !== "has-text-danger") {
+            return "has-text-danger";
+          } else {
+            return old;
+          }
+        });
+      } else {
+        setTimeClass(old => {
+          return old === "has-text-success" ? old : "has-text-success";
+        });
+      }
+      const seconds = timeSinceLift.seconds();
+      setDisplayTime(
+        `${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span className={timeClass}>{displayTime}</span>;
+};
 
 export default ({
   user,
@@ -53,35 +101,43 @@ export default ({
 
   return (
     <>
-      {dateGroups.map(([dateString, lifts]: [string, t.DisplayLift[]]) => {
-        return (
-          <div key={dateString}>
-            <div className="title is-5">{dateString}</div>
-            <table className="table is-striped is-fullwidth">
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Weight</th>
-                  <th>Reps</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {lifts.map(lift => (
-                  <tr key={lift.uid}>
-                    <td>{lift.date.toLocaleTimeString()}</td>
-                    <td>{lift.weight}</td>
-                    <td>{lift.reps}</td>
-                    <td>
-                      <Link to={`/lift/${lift.uid}/edit`}>Edit</Link>
-                    </td>
+      {dateGroups.map(
+        ([dateString, lifts]: [string, t.DisplayLift[]], groupIdx) => {
+          return (
+            <div key={dateString}>
+              <div className="title is-5">{dateString}</div>
+              <table className="table is-striped is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Weight</th>
+                    <th>Reps</th>
+                    <th></th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        );
-      })}
+                </thead>
+                <tbody>
+                  {lifts.map((lift, liftIdx) => (
+                    <tr key={lift.uid}>
+                      {liftIdx === 0 && groupIdx === 0 ? (
+                        <td>
+                          <TimeSince time={lift.date} />
+                        </td>
+                      ) : (
+                        <td>{lift.date.toLocaleTimeString()}</td>
+                      )}
+                      <td>{lift.weight}</td>
+                      <td>{lift.reps}</td>
+                      <td>
+                        <Link to={`/lift/${lift.uid}/edit`}>Edit</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          );
+        }
+      )}
     </>
   );
 };
