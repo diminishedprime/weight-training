0e90e77743831cbc8ea8a004a2b4ada737e5aa9f
Matt Hamrick <matthewjhamrick@gmail.com>
2020-01-06 00:52:09 +0000

    fixed a date format bug and removed excess table.

--- src/components/LiftTable.tsx
+++ src/components/LiftTable.tsx
@@ -7,7 +7,7 @@ import moment from "moment";
 
 const TimeSince = ({ time }: { time: Date }) => {
   const [displayTime, setDisplayTime] = React.useState(
-    moment(time).format("HH:MM")
+    moment(time).format("HH:mm")
   );
   const [timeClass, setTimeClass] = React.useState("");
   React.useEffect(() => {
@@ -19,7 +19,7 @@ const TimeSince = ({ time }: { time: Date }) => {
       );
       const minutes = timeSinceLift.minutes();
       if (minutes >= 15 || timeSinceLift.asMinutes() >= 15) {
-        setDisplayTime(moment(time).format("HH:MM"));
+        setDisplayTime(moment(time).format("HH:mm"));
         setTimeClass("");
         clearInterval(interval);
         return;
@@ -51,58 +51,32 @@ const TimeSince = ({ time }: { time: Date }) => {
 };
 
 export default ({ user, liftType }: { user: t.User; liftType: t.LiftType }) => {
-  const [lifts, setLifts] = React.useState<{ [date: string]: t.DisplayLift[] }>(
-    {}
-  );
-  const [allLifts, setAllLifts] = React.useState<t.DisplayLift[]>([]);
+  const [lifts, setLifts] = React.useState<t.DisplayLift[]>([]);
 
   React.useEffect(() => {
     return db.getLiftsOnSnapshot(
       firebase.firestore(),
       user,
       query => query.where("type", "==", liftType).orderBy("date", "desc"),
-      lifts => {
-        setAllLifts(lifts);
-        const grouped = lifts.reduce((grouping, lift) => {
-          const liftKey = lift.date.toLocaleDateString().slice(0, 10);
-          if (grouping[liftKey] === undefined) {
-            grouping[liftKey] = [];
-          }
-          grouping[liftKey].push(lift);
-          return grouping;
-        }, {} as { [date: string]: t.DisplayLift[] });
-        setLifts(grouped);
-      }
+      setLifts
     );
   }, [user, liftType]);
 
-  if (Object.keys(lifts).length === 0) {
+  if (lifts.length === 0) {
     return <div>No lifts recorded.</div>;
   }
-
-  const dateGroups: [string, t.DisplayLift[]][] = Object.keys(
-    lifts
-  ).map(key => [key, lifts[key]]);
 
   let seenDates = new Set();
 
   return (
-    <>
-      <table className="table is-striped is-fullwidth">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Weight</th>
-            <th>Reps</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {allLifts.map((lift, liftIdx) => {
-            const date = lift.date.toLocaleDateString().substring(0, 10);
-            let headingRow;
-            if (!seenDates.has(date)) {
-              headingRow = (
+    <table className="table is-striped is-fullwidth">
+      <tbody>
+        {lifts.map((lift, liftIdx) => {
+          const date = lift.date.toLocaleDateString().substring(0, 10);
+          let headingRow;
+          if (!seenDates.has(date)) {
+            headingRow = (
+              <>
                 <tr>
                   <td
                     align="center"
@@ -112,68 +86,37 @@ export default ({ user, liftType }: { user: t.User; liftType: t.LiftType }) => {
                     {date}
                   </td>
                 </tr>
-              );
-              seenDates.add(date);
-            }
-            return (
-              <React.Fragment key={lift.uid}>
-                {headingRow && headingRow}
-                <tr key={lift.uid}>
-                  {liftIdx === 0 ? (
-                    <td>
-                      <TimeSince time={lift.date} />
-                    </td>
-                  ) : (
-                    <td>{moment(lift.date).format("HH:mm")}</td>
-                  )}
-                  <td>{lift.weight}</td>
-                  <td>{lift.reps}</td>
-                  <td>
-                    <Link to={`/lift/${lift.uid}/edit`}>Edit</Link>
-                  </td>
+                <tr>
+                  <th>Date</th>
+                  <th>Weight</th>
+                  <th>Reps</th>
+                  <th></th>
                 </tr>
-              </React.Fragment>
+              </>
             );
-          })}
-        </tbody>
-      </table>
-      {dateGroups.map(
-        ([dateString, lifts]: [string, t.DisplayLift[]], groupIdx) => {
+            seenDates.add(date);
+          }
           return (
-            <div key={dateString}>
-              <div className="title is-5">{dateString}</div>
-              <table className="table is-striped is-fullwidth">
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Weight</th>
-                    <th>Reps</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {lifts.map((lift, liftIdx) => (
-                    <tr key={lift.uid}>
-                      {liftIdx === 0 && groupIdx === 0 ? (
-                        <td>
-                          <TimeSince time={lift.date} />
-                        </td>
-                      ) : (
-                        <td>{lift.date.toLocaleTimeString()}</td>
-                      )}
-                      <td>{lift.weight}</td>
-                      <td>{lift.reps}</td>
-                      <td>
-                        <Link to={`/lift/${lift.uid}/edit`}>Edit</Link>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+            <React.Fragment key={lift.uid}>
+              {headingRow && headingRow}
+              <tr key={lift.uid}>
+                {liftIdx === 0 ? (
+                  <td>
+                    <TimeSince time={lift.date} />
+                  </td>
+                ) : (
+                  <td>{moment(lift.date).format("HH:mm")}</td>
+                )}
+                <td>{lift.weight}</td>
+                <td>{lift.reps}</td>
+                <td>
+                  <Link to={`/lift/${lift.uid}/edit`}>Edit</Link>
+                </td>
+              </tr>
+            </React.Fragment>
           );
-        }
-      )}
-    </>
+        })}
+      </tbody>
+    </table>
   );
 };
