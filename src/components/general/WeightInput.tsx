57109670919332acf9d0053ac2a2e2e88a90ac97
Matt Hamrick <matthewjhamrick@gmail.com>
2020-01-17 21:50:12 -0800

    make weight input be fancier when switching between units.

--- src/components/general/WeightInput.tsx
+++ src/components/general/WeightInput.tsx
@@ -25,21 +25,51 @@ export const WeightInput: React.FC<WeightInput> = ({
     weight?.unit || defaultUnit
   );
   const [value, setValue] = React.useState<number | undefined>(weight?.value);
+  const hasInitialized = React.useRef(false);
+
+  // Normalize kg/lbs value to 1 decimal place.
+  React.useEffect(() => {
+    if (value !== undefined) {
+      const asString = value.toString();
+      const withDecimal = value.toFixed(1);
+      if (asString.indexOf(".") !== -1 && withDecimal !== asString) {
+        setValue(parseFloat(withDecimal));
+      }
+    }
+  }, [value]);
 
   React.useEffect(() => {
-    if (weight !== undefined) {
+    // Initalize if weight becomes not undefined.
+    if (weight !== undefined && hasInitialized.current === false) {
       setValue(weight.value);
       setUnit(weight.unit);
+      hasInitialized.current = true;
     }
   }, [weight]);
 
   React.useEffect(() => {
     if (value !== undefined) {
-      setWeight(new t.Weight(value, unit));
-    } else {
-      setWeight(undefined);
+      setWeight((old) => {
+        if (old === undefined || old.value === value) {
+          return old;
+        }
+        const nu = old.clone();
+        nu.value = value;
+        return nu;
+      });
     }
-  }, [unit, value, setWeight]);
+  }, [value]);
+
+  // If unit changes, weight should change accordingly.
+  React.useEffect(() => {
+    setWeight((old) => {
+      const nu = old?.clone().toUnit(unit);
+      if (nu !== undefined) {
+        setValue(nu.value);
+      }
+      return nu;
+    });
+  }, [unit, setWeight]);
 
   return (
     <div className="field has-addons has-addons-right flex-grow">
@@ -57,6 +87,16 @@ export const WeightInput: React.FC<WeightInput> = ({
             }
           }}
           onBlur={() => {
+            setValue((old) => {
+              if (
+                old === undefined ||
+                new t.Weight(old, unit).lessThanEq(t.Weight.bar())
+              ) {
+                return t.Weight.bar().toUnit(unit).value;
+              } else {
+                return old;
+              }
+            });
             setWeight((old) =>
               old === undefined || old.lessThanEq(t.Weight.bar())
                 ? t.Weight.bar()
