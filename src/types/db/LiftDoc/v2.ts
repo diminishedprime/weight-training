41070836712a1d05e461e4805534c5446c0e8fe9
Matt Hamrick <matthewjhamrick@gmail.com>
2020-02-01 23:27:51 -0800

    slowly getting there.

---
+++ src/types/db/LiftDoc/v2.ts
@@ -0,0 +1,68 @@
+import * as t from "../../../types";
+import { WeightField } from "../WeightField";
+
+export enum LiftDocType {
+  BARBELL = "barbell"
+}
+
+interface DeadliftDoc {
+  date: t.FirestoreTimestamp;
+  weight: WeightField;
+  type: t.LiftType.DEADLIFT;
+  reps: number;
+  warmup: boolean | undefined;
+  version: "1";
+  liftDocType: LiftDocType.BARBELL;
+  liftDocVersion: "1";
+}
+
+interface SquatDoc {
+  date: t.FirestoreTimestamp;
+  weight: WeightField;
+  type: t.LiftType.SQUAT;
+  reps: number;
+  warmup: boolean | undefined;
+  version: "1";
+  liftDocType: LiftDocType.BARBELL;
+  liftDocVersion: "1";
+}
+
+interface FrontSquatDoc {
+  date: t.FirestoreTimestamp;
+  weight: WeightField;
+  type: t.LiftType.FRONT_SQUAT;
+  reps: number;
+  warmup: boolean | undefined;
+  version: "1";
+  liftDocType: LiftDocType.BARBELL;
+  liftDocVersion: "1";
+}
+
+interface BenchPressDoc {
+  date: t.FirestoreTimestamp;
+  weight: WeightField;
+  type: t.LiftType.BENCH_PRESS;
+  reps: number;
+  warmup: boolean | undefined;
+  version: "1";
+  liftDocType: LiftDocType.BARBELL;
+  liftDocVersion: "1";
+}
+
+interface OverheadPressDoc {
+  date: t.FirestoreTimestamp;
+  weight: WeightField;
+  type: t.LiftType.OVERHEAD_PRESS;
+  reps: number;
+  warmup: boolean | undefined;
+  version: "1";
+  liftDocType: LiftDocType.BARBELL;
+  liftDocVersion: "1";
+}
+
+export type LiftDoc =
+  | DeadliftDoc
+  | SquatDoc
+  | FrontSquatDoc
+  | BenchPressDoc
+  | OverheadPressDoc;
