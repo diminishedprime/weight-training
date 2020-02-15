57bebe050fc97c5faac09ca60db84c583cd10243
Matt Hamrick <matthewjhamrick@gmail.com>
2020-02-15 14:20:32 -0800

    pulled out row progress into hook.

--- src/pages/Programs/BarbellTable.tsx
+++ src/pages/Programs/BarbellTable.tsx
@@ -5,6 +5,7 @@ import Bar from "../../components/Bar";
 import * as db from "../../db";
 import * as hooks from "../../hooks";
 import { BarbellLift, FirebaseUser, Lift } from "../../types";
+import usePersistentRowProgress from "./usePersistentRowProgress";
 
 interface BarbellRowProps {
   selected: boolean;
@@ -62,112 +63,19 @@ const BarbellTable: React.FC<BarbellTableProps> = ({
   isActive,
   user
 }) => {
-  const currentKey = localStorageKey + "-current";
-  const skippedKey = localStorageKey + "-skipped";
-  const finishedKey = localStorageKey + "-finished";
-  const [current, setCurrent] = React.useState(() => {
-    const currentString = window.localStorage.getItem(currentKey);
-    if (currentString === null) {
-      return 0;
-    } else {
-      return parseInt(currentString, 0);
-    }
-  });
-  const [skipped, setSkipped] = React.useState<Set<number>>(() => {
-    const skippedString = window.localStorage.getItem(skippedKey);
-    if (skippedString === null) {
-      return new Set();
-    } else {
-      return new Set(JSON.parse(skippedString));
-    }
-  });
-  const [finished, setFinished] = React.useState<Set<number>>(() => {
-    const finishedString = window.localStorage.getItem(finishedKey);
-    if (finishedString === null) {
-      return new Set();
-    } else {
-      return new Set(JSON.parse(finishedString));
-    }
-  });
-
-  React.useEffect(() => {
-    if (current < rows.length) {
-      window.localStorage.setItem(finishedKey, JSON.stringify([...finished]));
-      window.localStorage.setItem(skippedKey, JSON.stringify([...skipped]));
-      window.localStorage.setItem(currentKey, current.toString());
-    }
-  }, [
-    finished,
+  const {
     skipped,
+    skipRemaining,
+    skipLift,
     current,
-    currentKey,
-    finishedKey,
-    skippedKey,
-    rows.length
-  ]);
-
-  React.useEffect(() => {
-    if (current >= rows.length) {
-      window.localStorage.removeItem(finishedKey);
-      window.localStorage.removeItem(skippedKey);
-      window.localStorage.removeItem(currentKey);
-      finishSection();
-    }
-  }, [
-    current,
-    finishSection,
-    currentKey,
-    finishedKey,
-    skippedKey,
-    rows.length
-  ]);
-
-  const skipRemaining = React.useCallback(
-    (idx: number) => () => {
-      setSkipped((old) => {
-        for (let i = idx; i <= rows.length; i++) {
-          old.add(i);
-        }
-        return new Set(old);
-      });
-      setCurrent(rows.length);
-    },
-    [rows.length]
-  );
-
-  const skipLift = React.useCallback(
-    (idx: number) => () => {
-      setCurrent((old) => old + 1);
-      setSkipped((old) => {
-        old.add(idx);
-        return new Set(old);
-      });
-    },
-    []
+    finished,
+    finishLift
+  } = usePersistentRowProgress<BarbellLift>(
+    localStorageKey,
+    rows,
+    finishSection
   );
   const complete = current >= rows.length;
-
-  const finishLift = React.useCallback(
-    (idx: number, row: BarbellLift) => () => {
-      setCurrent((old) => old + 1);
-      setFinished((old) => {
-        old.add(idx);
-        return new Set(old);
-      });
-      db.addLift(
-        firebase.firestore(),
-        user,
-        Lift.forBarbellLift(
-          row.weight,
-          row.liftType,
-          row.reps,
-          row.warmup,
-          firebase.firestore.Timestamp.now()
-        )
-      );
-    },
-    [user]
-  );
 
   return (
     <table className="table">
@@ -211,7 +119,19 @@ const BarbellTable: React.FC<BarbellTableProps> = ({
                           Skip
                         </button>
                         <button
-                          onClick={finishLift(idx, row)}
+                          onClick={finishLift(idx, () => {
+                            db.addLift(
+                              firebase.firestore(),
+                              user,
+                              Lift.forBarbellLift(
+                                row.weight,
+                                row.liftType,
+                                row.reps,
+                                row.warmup,
+                                firebase.firestore.Timestamp.now()
+                              )
+                            );
+                          })}
                           className="button is-outlined is-success"
                         >
                           Done
