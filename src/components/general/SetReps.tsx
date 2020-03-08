1b2fd0fcf03fc07cbfe878c9743f9d5ceeefd3a2
Matt Hamrick <matthewjhamrick@gmail.com>
2020-03-07 22:12:41 -0800

    updated set reps to be material.

--- src/components/general/SetReps.tsx
+++ src/components/general/SetReps.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 interface SetRepsProps {
   setReps: React.Dispatch<React.SetStateAction<number>>;
@@ -7,32 +10,37 @@ interface SetRepsProps {
 
 export const SetReps: React.FC<SetRepsProps> = ({ setReps, reps }) => {
   return (
-    <div className="control">
-      <label className="label">Reps: {reps}</label>
-      <div className="buttons has-addons">
-        <button
-          className="button is-outlined is-danger"
-          onClick={() => setReps((a) => Math.max(1, a - 1))}
-        >
-          -
-        </button>
-        <button className="button" onClick={() => setReps(1)}>
-          1
-        </button>
-        <button className="button" onClick={() => setReps(3)}>
-          3
-        </button>
-        <button className="button" onClick={() => setReps(5)}>
-          5
-        </button>
-        <button
-          className="button is-outlined is-success"
-          onClick={() => setReps((a) => a + 1)}
-        >
-          +
-        </button>
-      </div>
-    </div>
+    <FormControlLabel
+      labelPlacement="top"
+      label={`Reps: ${reps}`}
+      control={
+        <ButtonGroup>
+          <Button onClick={() => setReps((a) => Math.max(1, a - 1))}>-</Button>
+          <Button
+            color={reps === 1 ? "primary" : undefined}
+            variant={reps === 1 ? "contained" : undefined}
+            onClick={() => setReps(1)}
+          >
+            1
+          </Button>
+          <Button
+            color={reps === 3 ? "primary" : undefined}
+            variant={reps === 3 ? "contained" : undefined}
+            onClick={() => setReps(3)}
+          >
+            3
+          </Button>
+          <Button
+            color={reps === 5 ? "primary" : undefined}
+            variant={reps === 5 ? "contained" : undefined}
+            onClick={() => setReps(5)}
+          >
+            5
+          </Button>
+          <Button onClick={() => setReps((a) => a + 1)}>+</Button>
+        </ButtonGroup>
+      }
+    />
   );
 };
 
