b9cd7f5f9af36046516959d99a50ca831de5c54b
Matt Hamrick <matthewjhamrick@gmail.com>
2021-11-18 11:42:39 -0800

    feat: add @material/ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
 
-import Title from '@/components/Title';
+import { Typography } from '@mui/material';
 
 const Home: React.FC<PageProps> = () => (
   <main>
-    <Title>Hello TypeScript!</Title>
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
+    <Typography variant="h1">Hello TypeScript!</Typography>
+    <Typography>
+      A TypeScript starter for Gatsby. Great for advanced users.
+    </Typography>
   </main>
 );
 
