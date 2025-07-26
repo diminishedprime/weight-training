f3bdb0cffbcbaa334da4d08001412c64cfd827c8
Matt Hamrick <matthewjhamrick@gmail.com>
2025-07-26 09:53:11 -0700

    misc: removed already finished todo

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -4,8 +4,6 @@ import PreferencesPage from "@/app/preferences/_page";
 import Breadcrumbs from "@/components/Breadcrumbs";
 import React, { Suspense } from "react";
 
-// TODO: when the user goes to /preferences from the top bar, we should keep
-// track of which page we were on so we can navigate back.
 export default async function SuspenseWrapper() {
   return (
     <React.Fragment>
