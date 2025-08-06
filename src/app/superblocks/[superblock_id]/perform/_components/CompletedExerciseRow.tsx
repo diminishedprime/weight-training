f00c0aefc785e43aa90bf407a6376ac48cc106fa
Matt Hamrick <matthewjhamrick@gmail.com>
2025-08-05 21:02:29 -0700

    feat: Allow edit on superblock (and a bit of cleanup) (#98)
    * feat: Allow edit on superblock (and a bit of cleanup)
    
    * fix: useMemo for links to avoid excessive re-rendering

--- src/app/superblocks/[superblock_id]/perform/_components/CompletedExerciseRow.tsx
+++ src/app/superblocks/[superblock_id]/perform/_components/CompletedExerciseRow.tsx
@@ -2,18 +2,21 @@ import UpdatePerceivedEffort from "@/app/superblocks/[superblock_id]/perform/_co
 import { UserPreferences } from "@/common-types";
 import { GetPerformSuperblockExercise } from "@/common-types/get-perform-superblock";
 import LabeledValue from "@/components/LabeledValue";
+import Link from "@/components/Link";
 import DisplayCompletionStatus from "@/components/display/DisplayCompletionStatus";
 import DisplayDuration from "@/components/display/DisplayDuration";
 import DisplayNotes from "@/components/display/DisplayNotes";
 import DisplayWeight from "@/components/display/DisplayWeight";
-import { Paper, Stack, Typography } from "@mui/material";
-import { useState } from "react";
+import { Paths, SearchParam, WithSearchParams } from "@/constants";
+import { Button, Paper, Stack, Typography } from "@mui/material";
+import { useMemo, useState } from "react";
 
 interface CompletedExerciseRowProps {
   userId: string;
   exercise: GetPerformSuperblockExercise;
   preferences: UserPreferences;
   setName: string;
+  currentPath: string;
 }
 
 const CompletedExerciseRow: React.FC<CompletedExerciseRowProps> = (props) => {
@@ -36,6 +39,16 @@ const CompletedExerciseRow: React.FC<CompletedExerciseRowProps> = (props) => {
         spacing={1}
         justifyContent="space-between"
       >
+        <Button
+          variant="outlined"
+          color="secondary"
+          size="small"
+          sx={{ alignSelf: "flex-end" }}
+          component={Link}
+          href={api.editLink}
+        >
+          Edit
+        </Button>
         <LabeledValue
           label={exercise.actual_weight_value ? "Actual Work " : "Target Work"}
         >
@@ -73,8 +86,27 @@ const CompletedExerciseRow: React.FC<CompletedExerciseRowProps> = (props) => {
 export default CompletedExerciseRow;
 
 const useCompletedExerciseRowAPI = (props: CompletedExerciseRowProps) => {
+  const {
+    exercise: { equipment_type, exercise_type, id },
+    currentPath,
+  } = props;
+
   const [perceivedEffort, setPerceivedEffort] = useState(
     props.exercise.perceived_effort ?? null,
   );
-  return { perceivedEffort, setPerceivedEffort };
+
+  const editLink = useMemo(
+    () =>
+      WithSearchParams(
+        Paths.Exercise_EquipmentType_ExerciseType_Edit_ExerciseId(
+          equipment_type,
+          exercise_type,
+          id,
+        ),
+        [SearchParam.BackTo, props.currentPath],
+      ),
+    [equipment_type, exercise_type, id, currentPath],
+  );
+
+  return { perceivedEffort, setPerceivedEffort, editLink };
 };
