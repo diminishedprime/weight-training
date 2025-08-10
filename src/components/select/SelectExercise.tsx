bbd9ec06983c189d7119223ba07f8fed22f4392f
Matt Hamrick <matthewjhamrick@gmail.com>
2025-08-09 20:19:53 -0700

    feat: cleanup exercise selector

--- src/components/select/SelectExercise.tsx
+++ src/components/select/SelectExercise.tsx
@@ -1,15 +1,20 @@
 "use client";
-import { EquipmentType, ExerciseType, ProgramDayType } from "@/common-types";
-import { EQUIPMENT_TYPES, EXERCISES_FOR_DAY_TYPE } from "@/constants";
+import { ExerciseType, ProgramDayType } from "@/common-types";
+import DisplayEquipmentThumbnail from "@/components/display/DisplayEquipmentThumbnail";
+import { EXERCISE_TYPES, EXERCISES_FOR_DAY_TYPE } from "@/constants";
 import { Constants } from "@/database.types";
+import { exerciseTypeUIStringBrief, programDayTypeUIString } from "@/uiStrings";
+import { equipmentForExercise } from "@/util";
+import ResetIcon from "@mui/icons-material/RestartAlt";
 import {
-  equipmentTypeUIString,
-  exerciseTypeUIStringBrief,
-  exerciseTypeUIStringLong,
-  programDayTypeUIString,
-} from "@/uiStrings";
-import { equipmentForExercise, EXERCISES_BY_EQUIPMENT } from "@/util";
-import { Autocomplete, Chip, Stack, TextField } from "@mui/material";
+  Autocomplete,
+  Box,
+  Chip,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Set as ImmutableSet } from "immutable";
 import React, { useCallback, useMemo, useState } from "react";
 
@@ -20,9 +25,6 @@ export interface SelectExerciseProps {
 
 const SelectExercise: React.FC<SelectExerciseProps> = (props) => {
   const api = useSelectExerciseAPI(props);
-
-  // TODO: easy if we change the chips, and the selected exercise is no longer valid,
-  // it should be cleared.
 
   return (
     <Stack spacing={1.5}>
@@ -55,33 +57,35 @@ const SelectExercise: React.FC<SelectExerciseProps> = (props) => {
           onClick={api.selectNoneProgramDayTypes}
         />
       </Stack>
-      <Stack spacing={1.25}>
-        <Autocomplete
-          fullWidth
-          size="small"
-          options={api.availableEquipment}
-          value={api.selectedEquipment}
-          onChange={(_, newValue) => api.handleEquipmentChange(newValue)}
-          getOptionLabel={(option) => equipmentTypeUIString(option)}
-          renderInput={(params) => (
-            <TextField {...params} label="Equipment Type" variant="outlined" />
-          )}
-        />
+      <Stack spacing={1} direction="row" useFlexGap>
         <Autocomplete
           fullWidth
           size="small"
           options={api.availableExercises.toArray()}
           value={props.exercise}
-          onChange={(_, newValue) => api.handleExerciseChange(newValue)}
-          getOptionLabel={(option) =>
-            api.selectedEquipment
-              ? exerciseTypeUIStringBrief(option)
-              : exerciseTypeUIStringLong(option)
-          }
+          onChange={(_, newValue) => props.setExercise((_) => newValue)}
+          getOptionLabel={(option) => exerciseTypeUIStringBrief(option)}
+          renderOption={(props, option) => (
+            <Box
+              component="li"
+              {...props}
+              sx={{ display: "flex", alignItems: "center", gap: 1 }}
+            >
+              <DisplayEquipmentThumbnail
+                size={20}
+                equipmentType={equipmentForExercise(option)}
+              />
+              <Typography>{exerciseTypeUIStringBrief(option)}</Typography>
+            </Box>
+          )}
+          blurOnSelect={true}
           renderInput={(params) => (
             <TextField {...params} label="Exercise" variant="outlined" />
           )}
         />
+        <IconButton onClick={() => props.setExercise(null)}>
+          <ResetIcon />
+        </IconButton>
       </Stack>
     </Stack>
   );
@@ -89,57 +93,24 @@ const SelectExercise: React.FC<SelectExerciseProps> = (props) => {
 
 export default SelectExercise;
 
-const useSelectExerciseAPI = (props: SelectExerciseProps) => {
-  const { exercise, setExercise } = props;
-
-  const [selectedEquipment, setSelectedEquipment] =
-    useState<EquipmentType | null>(
-      (exercise !== null && equipmentForExercise(exercise)) || null,
-    );
-
+const useSelectExerciseAPI = (_props: SelectExerciseProps) => {
   const [selectedProgramDayTypes, setSelectedProgramDayTypes] =
     useState<ImmutableSet<ProgramDayType>>(ImmutableSet());
 
-  const exercisesForSelectedDayTypes = useMemo(
-    () =>
-      selectedProgramDayTypes.reduce(
-        (acc, dayType) =>
-          acc.union(EXERCISES_FOR_DAY_TYPE.get(dayType, ImmutableSet())),
-        ImmutableSet() as ImmutableSet<ExerciseType>,
-      ),
-    [selectedProgramDayTypes],
-  );
+  const exercisesForSelectedDayTypes = useMemo(() => {
+    if (selectedProgramDayTypes.isEmpty()) {
+      return EXERCISE_TYPES;
+    }
+    return selectedProgramDayTypes.reduce(
+      (acc, dayType) =>
+        acc.union(EXERCISES_FOR_DAY_TYPE.get(dayType, ImmutableSet())),
+      ImmutableSet() as ImmutableSet<ExerciseType>,
+    );
+  }, [selectedProgramDayTypes]);
 
   const availableExercises: ImmutableSet<ExerciseType> = useMemo(() => {
-    if (!selectedEquipment) {
-      return exercisesForSelectedDayTypes;
-    }
-    const baseExercises: ImmutableSet<ExerciseType> =
-      EXERCISES_BY_EQUIPMENT.get(selectedEquipment, ImmutableSet());
-    return baseExercises.intersect(exercisesForSelectedDayTypes);
-  }, [selectedEquipment, exercisesForSelectedDayTypes]);
-
-  const handleExerciseChange = useCallback(
-    (newExercise: ExerciseType | null) => {
-      setExercise((_) => newExercise);
-      setSelectedEquipment((_) =>
-        newExercise ? equipmentForExercise(newExercise) : null,
-      );
-    },
-    [setExercise],
-  );
-
-  const handleEquipmentChange = useCallback(
-    (newEquipment: EquipmentType | null) => {
-      setSelectedEquipment((_) => newEquipment);
-      setExercise((_) =>
-        newEquipment
-          ? (EXERCISES_BY_EQUIPMENT.get(newEquipment)?.first() ?? null)
-          : null,
-      );
-    },
-    [setExercise],
-  );
+    return EXERCISE_TYPES.intersect(exercisesForSelectedDayTypes);
+  }, [exercisesForSelectedDayTypes]);
 
   const onChipClick = useCallback((dayType: ProgramDayType) => {
     setSelectedProgramDayTypes((old) =>
@@ -162,10 +133,6 @@ const useSelectExerciseAPI = (props: SelectExerciseProps) => {
     selectAllProgramDayTypes,
     onChipClick,
     selectedProgramDayTypes,
-    handleExerciseChange,
     availableExercises,
-    selectedEquipment,
-    handleEquipmentChange,
-    availableEquipment: EQUIPMENT_TYPES,
   };
 };
