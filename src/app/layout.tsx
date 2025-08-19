3147c435762f66c646015e9d1cdda06f89b6c967
Matt Hamrick <matthewjhamrick@gmail.com>
2025-08-18 21:15:24 -0700

    feat: update the favicon (#120)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default async function RootLayout({
   const themeOptions = await getThemeOptions();
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider themeOptions={themeOptions}>
